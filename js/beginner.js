1bf1fbccc009a403a7a1e9532558783bd9d9ee47
Ari Lerner <arilerner@mac.com>
2013-08-12 18:44:11 -0700

    Updated to part5

--- js/beginner.js
+++ js/beginner.js
@@ -3,12 +3,37 @@ var app = angular.module('myApp', []),
     nprUrl = 'http://api.npr.org/query?id=61&fields=relatedLink,title,byline,text,audio,image,pullQuote,all&output=JSON';
 
 
-app.controller('PlayerController', ['$scope', function($scope) {
+app.directive('nprLink', function() {
+  return {
+    restrict: 'EA',
+    require: ['^ngModel'],
+    replace: true,
+    scope: {
+      ngModel: '=',
+      play: '&'
+    },
+    templateUrl: 'views/nprListItem.html',
+    link: function(scope, ele, attr) {
+      scope.duration = scope.ngModel.audio[0].duration.$text;
+    }
+  }
+});
+
+app.controller('PlayerController', ['$scope', '$http', function($scope, $http) {
   var audio = document.createElement('audio');
   $scope.audio = audio;
 
-  audio.src = 'http://pd.npr.org/npr-mp4/npr/sf/2013/07/20130726_sf_05.mp4?orgId=1&topicId=1032&ft=3&f=61';
-  audio.play();
+  // audio.src = 'http://pd.npr.org/npr-mp4/npr/sf/2013/07/20130726_sf_05.mp4?orgId=1&topicId=1032&ft=3&f=61';
+  // audio.play();
+
+  $scope.play = function(program) {
+    if ($scope.playing) audio.pause();
+    var url = program.audio[0].format.mp4.$text;
+    audio.src = url;
+    audio.play();
+    // Store the state of the player as playing
+    $scope.playing = true;
+  }
 
   $http({
     method: 'JSONP',
